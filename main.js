ee4e3b5cd909a53111391e9831dc547947004ac7
zanadoman <domanseven@gmail.com>
2025-08-19 14:05:49 +0200

    electron setup

---
+++ main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { app, BrowserWindow, Menu } = require('electron/main');
+const path = require('path');
+const url = require('url');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  Menu.setApplicationMenu(null);
+
+  if (app.isPackaged) {
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, '/dist/Desktop/browser/index.html'),
+      protocol: 'file',
+      slashes: true
+    }));
+  } else {
+    win.loadURL('http://localhost:4200');
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
