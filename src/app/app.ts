ef263d0da98cedeb060ae6d01d6f76fd65f0a4a9
zanadoman <domanseven@gmail.com>
2025-08-24 18:18:58 +0200

    angular test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Desktop');
+  protected readonly count = signal(0);
+
+  protected decrement(): void {
+    this.count.update(value => value - 1);
+  }
+
+  protected increment(): void {
+    this.count.update(value => value + 1);
+  }
 }
